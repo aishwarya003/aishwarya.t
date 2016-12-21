1d67637abe0d9923048d43e65d3bb67c71aa938d
aishwarya003 <aishwarya.t2311@gmail.com>
2016-12-21 22:16:35 +0530

    Add files via upload

---
+++ nocharacter.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[100],count,i;
+printf("\nEnter a string");
+scanf("%s",&a[i]);
+for(i=0;a[i]!=NULL;i++)
+{
+count++;
+}
+printf("\n number of characters %d",count);
+}
