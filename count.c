cc10b12b13fa23c520529c1b4a7fdccebbd3bcbc
aishwarya003 <aishwarya.t2311@gmail.com>
2016-12-21 21:06:26 +0530

    Add files via upload

---
+++ count.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+int dig(int n)
+{
+ static int count;
+ n=n/10;
+ count++;
+ if(n!=0)
+ dig(n);
+ else
+   return(count);
+}
+void main()
+{
+int n,a,count;
+printf("\nEnter the digit");
+scanf("%d",&n);
+count=dig(n);
+
+printf("\nthe added digit is %d",a);
+}
