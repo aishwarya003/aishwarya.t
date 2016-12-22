13872189e7443b05d3a0b1abfd8573d8538679f5
aishwarya003 <aishwarya.t2311@gmail.com>
2016-12-22 09:36:58 +0530

    Add files via upload

---
+++ strtoint.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+int num;
+char a[100];
+printf("\nEnter a string");
+scanf("%s",&a);
+printf("\nthe converted string is %d",atoi(a));
+getch();
+
+}
