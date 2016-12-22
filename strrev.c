43810db75256b390ce1789e5a52786c517c00be6
aishwarya003 <aishwarya.t2311@gmail.com>
2016-12-22 10:13:24 +0530

    Add files via upload

---
+++ strrev.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[10],b;
+printf("\n Enter a string");
+scanf("%s",&a);
+
+printf("\nthe reversed string is %s",strrev(a));
+
+}
