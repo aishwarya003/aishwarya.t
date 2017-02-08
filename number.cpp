4d2b4064d94ca960c11599d3f849e011ce46ed7c
Aishwarya <aishwarya.t2311@gmail.com>
2017-02-08 20:04:01 +0530

    Add files via upload

---
+++ number.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+void find(int n)
+{
+   string arr[n+1];
+    arr[0] = "";
+    int size=1,m=1;
+     while (size <= n)
+    {
+        for (int i=0; i<m && (size+i)<=n; i++)
+            arr[size + i] = "3" +  arr[size - m + i];
+        for (int i=0; i<m && (size + m + i)<=n; i++)
+            arr[size + m + i] = "4" +  arr[size - m + i];
+
+       m=m<<1;
+
+
+        size = size + m;
+    }
+    cout << arr[n] << endl;
+}
+
+
+int main()
+{
+
+    for (int i = 1; i <100; i++)
+        find(i);
+    return 0;
+}
+
+
+
